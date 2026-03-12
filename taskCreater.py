9c49be7379d4fec4d34ae2d6282200f2bcd92705
Laura <23802255@student.uwa.edu.au>
2026-03-12 20:36:36 +0800

    fix syntax issue

--- taskCreater.py
+++ taskCreater.py
@@ -1,5 +1,7 @@
 def createTask():
     #Task created with placeholder values
+
     #Index 3 is for priority, Index 4 is for task length
-    new_task = [Index,Task Name,Task Length, 0, 0]
+    new_task = [,,, 0, 0]
+
 
