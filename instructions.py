734bd5979b0e7427ba39744700e0c1bc03f47b03
Laura <23802255@student.uwa.edu.au>
2026-03-12 18:54:07 +0800

    Updated instructions so that the printing is inside the function showInstructions.

--- instructions.py
+++ instructions.py
@@ -1,14 +1,15 @@
-print("""Welcome to the Pomodoriser Tool!
+def showInstructions():
+    print("""Welcome to the Pomodoriser Tool!
 
-Here are the instructions of how to use it.
+    Here are the instructions of how to use it.
 
--------------------------------------------------
-Action              | Command  
--------------------------------------------------    
-Add new task        | A + Enter
--------------------------------------------------    
-Generate plan       | F + Enter
--------------------------------------------------  
-Create new plan     | N + Enter
--------------------------------------------------
+    -------------------------------------------------
+    Action              | Command  
+    -------------------------------------------------    
+    Add new task        | A + Enter
+    -------------------------------------------------    
+    Generate plan       | F + Enter
+    -------------------------------------------------  
+    Create new plan     | N + Enter
+    -------------------------------------------------
       """)
