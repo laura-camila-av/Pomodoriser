7d3bd7cb7d23118ebea34da90b0febd3f3f34da3
Laura <23802255@student.uwa.edu.au>
2026-03-12 20:47:28 +0800

    Changed checkInputLength logic

--- taskPopulator.py
+++ taskPopulator.py
@@ -1,15 +1,16 @@
 #function to check the input doesn't exceed the maximum length capacity (to protect neat table formatting)
 
-exceedsMaxLength = False
 
 def checkInputLength(field):
     if len(field) > 50:
-        exceedsMaxLength = True
+        return True
+    else:
+        return False
     
 
 def requestInput():
     taskName = str(input("Enter a brief description of the task you wish to complete then click enter."))
-    checkInputLength(taskName)
+    exceedsMaxLength = checkInputLength(taskName)
     while exceedsMaxLength:
         print("Your description is too long. PLease re-enter a description that does not exceed 50 characters.")
         taskName = str(input("Enter a brief description of the task you wish to complete then click enter."))
