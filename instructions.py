d7f649afd5a678bd6b65babde2f410eb0fc18e11
Laura <23802255@student.uwa.edu.au>
2026-03-12 18:36:22 +0800

    Added V1 of instructions to instructions.py

--- instructions.py
+++ instructions.py
@@ -1,1 +1,14 @@
-print("Pomodoriser instructions to be inserted here.")
+print("""Welcome to the Pomodoriser Tool!
+
+Here are the instructions of how to use it.
+
+-------------------------------------------------
+Action              | Command  
+-------------------------------------------------    
+Add new task        | A + Enter
+-------------------------------------------------    
+Generate plan       | F + Enter
+-------------------------------------------------  
+Create new plan     | N + Enter
+-------------------------------------------------
+      """)
