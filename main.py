39eb8340df170fe92ef0872df7f24ce7da504372
Laura <23802255@student.uwa.edu.au>
2026-03-13 15:27:27 +0800

    Added call to priority function

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ instructions.showInstructions()
 taskPopulator.requestTaskName()
 
 taskPopulator.requestTaskTime()
+
+taskPopulator.requestTaskPriority()
 #Eventually could break down insert attribute so tasks are added once at a time.
 
 taskPopulator.insertAtribute()
