f2f31d3b430c404b4d958bc5826d12298cb588f4
Laura <23802255@student.uwa.edu.au>
2026-03-13 15:40:11 +0800

    update

--- main.py
+++ main.py
@@ -5,18 +5,12 @@ import taskPopulator
 
 instructions.showInstructions()
 
-
-#taskCreater.createTask()
-
-#taskPopulator.createTask()
-
 taskPopulator.requestTaskName()
 
 taskPopulator.requestTaskTime()
 
 taskPopulator.requestTaskPriority()
 #Eventually could break down insert attribute so tasks are added once at a time.
-
 taskPopulator.insertAtribute()
 
 
