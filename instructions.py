3b48b2e4782eb88ef9652a0822a98b84b187e5ca
laura-camila-av <168151864+laura-camila-av@users.noreply.github.com>
2026-03-22 19:44:15 +0800

    Create instructions.py

---
+++ instructions.py
@@ -0,0 +1,16 @@
+def showInstructions():
+    print("""Welcome to the Pomodoriser Tool!
+
+    Here are the instructions of how to use it.
+
+    --------------------+----------------------------
+    Action              | Command  
+    --------------------+----------------------------    
+    Add new task        | C + Enter
+    --------------------+----------------------------    
+    Generate plan       | G + Enter
+    --------------------+----------------------------  
+      """)
+    
+    
+    
